2fe7c0e134cbf8eb87153faf3c3477e06e2c6e42
Elizabeth <foxcapade@gmail.com>
2023-01-20 10:29:22 -0500

    updates: * add service config json schema * add example service config draft * add asciidoc gen of design doc * update design doc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,26 @@
+tasks.create("compile-design-doc") {
+  group = "monorepo"
+
+  doLast {
+    val command = arrayOf(
+      "asciidoctor",
+      "--backend", "html5",
+      "--out-file", "docs/design/1.0/design.html",
+      "docs/design/1.0/design.adoc"
+    )
+
+    println("Running ASCIIDoctor")
+    println(command.joinToString(" "))
+
+    with(ProcessBuilder(*command).start()) {
+      if (waitFor() != 0) {
+        errorStream.bufferedReader().use { it.lines().forEach(::println) }
+        throw RuntimeException("ASCIIDoctor command execution failed.")
+      }
+    }
+  }
+}
+
 tasks.create("generate-raml-docs") {
   group = "monorepo"
 
