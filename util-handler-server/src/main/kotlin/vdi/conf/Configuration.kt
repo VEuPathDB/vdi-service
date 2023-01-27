1f85cf99bff80210ef7388a312f6689232a8a28f
Elizabeth <foxcapade@gmail.com>
2023-01-27 15:00:17 -0500

    updates

--- util-handler-server/src/main/kotlin/vdi/conf/Configuration.kt
+++ util-handler-server/src/main/kotlin/vdi/conf/Configuration.kt
@@ -1,8 +1,21 @@
 package vdi.conf
 
+/**
+ * VDI Handler Service Root Configuration
+ *
+ * This configuration object is a global container for the configuration values
+ * set on the environment for the VDI handler service.
+ */
 object Configuration {
+
+  /**
+   * HTTP server specific configuration values.
+   */
   val serverConfiguration = parseServerConfig()
 
+  /**
+   * Database connection configuration values.
+   */
   val databaseConfigurations = parseDatabaseConfigs()
 }
 
