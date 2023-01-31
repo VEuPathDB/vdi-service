347eacfc9baaccde9d909dc8fe4a825d2fc3229e
Elizabeth <foxcapade@gmail.com>
2023-01-31 16:37:58 -0500

    updates

--- util-handler-server/src/main/kotlin/vdi/util/LoggingOutputStream.kt
+++ util-handler-server/src/main/kotlin/vdi/util/LoggingOutputStream.kt
@@ -5,6 +5,18 @@ import java.io.OutputStream
 
 private const val ASCII_LF = 0x0A
 
+/**
+ * Logging Output Stream
+ *
+ * An [OutputStream] implementation that writes lines of text to the given
+ * [Logger] as they are written.
+ *
+ * All log lines are written to the logger via the [Logger.info] method.
+ *
+ * @constructor Constructs a new `LoggingOutputStream` instance.
+ *
+ * @param log `Logger` instance that the written bytes will be logged to.
+ */
 class LoggingOutputStream(private val log: Logger) : OutputStream() {
   private val buffer = StringBuilder(1024)
 
