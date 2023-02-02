ef809ae12b69117b7abf48b44d5f65ef207b9c7b
Elizabeth <foxcapade@gmail.com>
2023-02-02 10:51:03 -0500

    updates

--- util-handler-server/src/main/kotlin/vdi/server/model/ImportDetails.kt
+++ util-handler-server/src/main/kotlin/vdi/server/model/ImportDetails.kt
@@ -1,10 +1,12 @@
 package vdi.server.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import vdi.Const.FieldName
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ImportDetails(
   @JsonProperty(FieldName.VDIID)
   val vdiID: String,
