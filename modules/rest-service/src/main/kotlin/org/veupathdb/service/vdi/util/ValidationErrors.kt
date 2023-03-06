afb44531b88029e338291f15d6acccefc6a42497
Elizabeth <foxcapade@gmail.com>
2023-03-06 18:50:49 -0500

    updates

--- modules/rest-service/src/main/kotlin/org/veupathdb/service/vdi/util/ValidationErrors.kt
+++ modules/rest-service/src/main/kotlin/org/veupathdb/service/vdi/util/ValidationErrors.kt
@@ -5,22 +5,44 @@ import com.fasterxml.jackson.databind.JsonNode
 import org.veupathdb.lib.container.jaxrs.errors.UnprocessableEntityException
 import vdi.components.json.JSON
 
-// TODO: relocate me
+/**
+ * Request Validation Error Bundle
+ *
+ * Container type for collecting errors while validating an incoming request.
+ *
+ * Errors are collected into two categories, general errors, and errors by key.
+ * General errors are collected as a list, while by key errors are collected as
+ * a map of lists by a given key.
+ *
+ * @author Elizabeth Paige Harper - https://github.com/foxcapades
+ */
 class ValidationErrors {
   private val byKey   = HashMap<String, MutableList<String>>()
   private val general = ArrayList<String>()
 
+  /**
+   * `true` if this [ValidationErrors] instance contains no errors.
+   */
   val isEmpty
     get() = byKey.isEmpty() && general.isEmpty()
 
+  /**
+   * `true` if this [ValidationErrors] instance contains one or more errors.
+   */
   val isNotEmpty
     get() = byKey.isNotEmpty() || general.isNotEmpty()
 
+  /**
+   * Adds a keyed error to this error bundle.
+   */
   fun add(key: String, error: String) {
     byKey.computeIfAbsent(key) { ArrayList() }
       .add(error)
   }
 
+  /**
+   * Adds a general error to this error bundle.
+   */
   fun add(error: String) {
     general.add(error)
   }
