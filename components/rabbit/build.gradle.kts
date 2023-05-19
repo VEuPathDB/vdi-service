97c9f5aec1f221a8920145e8a3c239237ea31791
Elizabeth <foxcapade@gmail.com>
2023-05-19 14:53:06 -0400

    update dependency

--- components/rabbit/build.gradle.kts
+++ components/rabbit/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
   implementation("org.veupathdb.vdi:vdi-component-common:1.0.0-SNAPSHOT") { isChanging = true }
   implementation("org.slf4j:slf4j-api:1.7.36")
   implementation("com.rabbitmq:amqp-client:5.17.0")
