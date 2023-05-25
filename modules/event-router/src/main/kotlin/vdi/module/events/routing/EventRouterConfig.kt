d5dc996eb1782126f13039bc19e1d125e91a0992
Elizabeth <foxcapade@gmail.com>
2023-05-25 04:52:37 -0400

    simplify config

---
+++ modules/event-router/src/main/kotlin/vdi/module/events/routing/EventRouterConfig.kt
@@ -0,0 +1,22 @@
+package vdi.module.events.routing
+
+import org.veupathdb.vdi.lib.common.env.EnvKey
+import org.veupathdb.vdi.lib.common.env.Environment
+import org.veupathdb.vdi.lib.common.env.require
+import org.veupathdb.vdi.lib.kafka.router.KafkaRouterConfig
+import org.veupathdb.vdi.lib.rabbit.RabbitMQConfig
+
+data class EventRouterConfig(
+  val rabbitConfig: RabbitMQConfig,
+  val s3Bucket: String,
+  val kafkaConfig: KafkaRouterConfig
+) {
+  constructor() : this(System.getenv())
+
+  constructor(env: Environment) : this(
+    rabbitConfig = RabbitMQConfig(env),
+    s3Bucket     = env.require(EnvKey.S3.BucketName),
+    kafkaConfig  = KafkaRouterConfig(env)
+  )
+}
+
