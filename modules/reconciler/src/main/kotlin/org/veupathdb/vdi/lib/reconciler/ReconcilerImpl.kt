95a7d13d00cbf515f6462012b8ca6595e9b1d389
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2023-11-06 13:12:37 -0500

    reformat file

--- modules/reconciler/src/main/kotlin/org/veupathdb/vdi/lib/reconciler/ReconcilerImpl.kt
+++ modules/reconciler/src/main/kotlin/org/veupathdb/vdi/lib/reconciler/ReconcilerImpl.kt
@@ -13,58 +13,58 @@ import vdi.component.modules.VDIServiceModuleBase
 import java.util.*
 
 class ReconcilerImpl(private val config: ReconcilerConfig) :
-    Reconciler, VDIServiceModuleBase("reconciler") {
+  Reconciler, VDIServiceModuleBase("reconciler") {
 
-    override suspend fun run() {
-        if (!config.reconcilerEnabled) {
-            logger().warn("Reconciler is disabled. Skipping run")
-            return
-        }
-        logger().info("Running ReconcilerImpl module")
-        val datasetManager = DatasetManager(requireS3Bucket(requireS3Client(config.s3Config), config.s3Bucket))
-        val kafkaRouter = requireKafkaRouter()
-
-        val targets: MutableList<ReconcilerInstance> = AppDatabaseRegistry.iterator().asSequence()
-            .map { (project, _) ->
-                ReconcilerInstance(
-                    AppDBTarget(project, project),
-                    datasetManager,
-                    kafkaRouter
-                )
-            }
-            .toMutableList()
-        targets.add(ReconcilerInstance(CacheDBTarget(), datasetManager, kafkaRouter))
-
-        runBlocking {
-            while (!isShutDown()) {
-                logger().info("Scheduling reconciler for ${targets.size} targets.")
-
-                val timer = Metrics.reconcilerTimes.startTimer()
-
-                // Schedule the reconciler for each target database.
-                targets.forEach { target ->
-                    launch(
-                        CoroutineThreadContext(
-                            contextData = ThreadContextData( // Add log4j context to reconciler to distinguish targets in logs.
-                                map = mapOf(
-                                    Pair(
-                                        "reconciler",
-                                        target.name
-                                    )
-                                ), Stack()
-                            )
-                        )
-                    ) { target.reconcile() }
-                }
-
-                timer.observeDuration()
-
-                delay(config.runInterval.toMillis())
-            }
-        }
+  override suspend fun run() {
+    if (!config.reconcilerEnabled) {
+      logger().warn("Reconciler is disabled. Skipping run")
+      return
     }
+    logger().info("Running ReconcilerImpl module")
+    val datasetManager = DatasetManager(requireS3Bucket(requireS3Client(config.s3Config), config.s3Bucket))
+    val kafkaRouter = requireKafkaRouter()
 
-    private suspend fun requireKafkaRouter() = safeExec("failed to create KafkaRouter instance") {
-        KafkaRouterFactory(config.kafkaRouterConfig).newKafkaRouter()
+    val targets: MutableList<ReconcilerInstance> = AppDatabaseRegistry.iterator().asSequence()
+      .map { (project, _) ->
+        ReconcilerInstance(
+          AppDBTarget(project, project),
+          datasetManager,
+          kafkaRouter
+        )
+      }
+      .toMutableList()
+    targets.add(ReconcilerInstance(CacheDBTarget(), datasetManager, kafkaRouter))
+
+    runBlocking {
+      while (!isShutDown()) {
+        logger().info("Scheduling reconciler for ${targets.size} targets.")
+
+        val timer = Metrics.reconcilerTimes.startTimer()
+
+        // Schedule the reconciler for each target database.
+        targets.forEach { target ->
+          launch(
+            CoroutineThreadContext(
+              contextData = ThreadContextData( // Add log4j context to reconciler to distinguish targets in logs.
+                map = mapOf(
+                  Pair(
+                    "reconciler",
+                    target.name
+                  )
+                ), Stack()
+              )
+            )
+          ) { target.reconcile() }
+        }
+
+        timer.observeDuration()
+
+        delay(config.runInterval.toMillis())
+      }
     }
+  }
+
+  private suspend fun requireKafkaRouter() = safeExec("failed to create KafkaRouter instance") {
+    KafkaRouterFactory(config.kafkaRouterConfig).newKafkaRouter()
+  }
 }
