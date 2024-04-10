65482da5b3946ee901fb06c637674b0a8bfe8aed
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-04-10 13:42:25 -0400

    update dependency to include bug fix

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     api("org.veupathdb.lib:multipart-jackson-pojo:1.1.3")
 
     // VDI
-    api("org.veupathdb.vdi:vdi-component-common:10.0.0")
+    api("org.veupathdb.vdi:vdi-component-common:10.0.1")
     api("org.veupathdb.vdi:vdi-component-json:1.0.1")
 
     // Database
