67e751bc2acefe4dec658476fcf3d55b76b6f70a
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-04-19 13:37:37 -0400

    remove httpclient logging the httpclient logs happen on a separate thread and do not include a dataset id making it impossible to reliably link those logs to any particular vdi process

--- startup.sh
+++ startup.sh
@@ -16,4 +16,4 @@ waitFor cache-db cache-db 5432
 waitFor rabbit "$GLOBAL_RABBIT_HOST" "${GLOBAL_RABBIT_PORT:-5672}"
 waitFor minio "$S3_HOST" "$S3_PORT"
 
-exec java -jar -XX:+CrashOnOutOfMemoryError $JVM_MEM_ARGS $JVM_ARGS -Djdk.httpclient.HttpClient.log=errors,headers /service.jar
+exec java -jar -XX:+CrashOnOutOfMemoryError $JVM_MEM_ARGS $JVM_ARGS /service.jar
