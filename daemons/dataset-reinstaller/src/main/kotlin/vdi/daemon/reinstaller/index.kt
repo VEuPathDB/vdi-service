376c0e0a8fbdc23013650b6e308a5c82039fc56d
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-04-22 15:05:12 -0400

    remove dataset reinstaller daemon

--- daemons/dataset-reinstaller/src/main/kotlin/vdi/daemon/reinstaller/index.kt
+++
@@ -1,7 +0,0 @@
-package vdi.daemon.reinstaller
-
-fun DatasetReinstaller(
-  abortCB: (String?) -> Nothing,
-  config: DatasetReinstallerConfig = DatasetReinstallerConfig(),
-): DatasetReinstaller =
-  DatasetReinstallerImpl(config, abortCB)
