7e6ee348d5de4ccbc5478925aae9d5bb01bacba7
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-04-23 14:02:58 -0400

    ref #257

--- service/lane/reconciliation/src/main/kotlin/vdi/lane/reconciliation/DatasetReconciler.kt
+++ service/lane/reconciliation/src/main/kotlin/vdi/lane/reconciliation/DatasetReconciler.kt
@@ -77,6 +77,10 @@ internal class DatasetReconciler(
     if (!haveImportableFile())
       logError("missing import-ready file")
 
+    if (reimport == ReimportIndicator.ReimportNotNeeded)
+      if (getCacheImportControl() == DatasetImportStatus.Queued)
+        updateCacheDBImportStatus(DatasetImportStatus.Complete)
+
     if (reimport != ReimportIndicator.ReimportNotPossible) {
       if (!haveInstallableFile())
         logError("missing install-ready file")
@@ -226,8 +230,26 @@ internal class DatasetReconciler(
     }
 
   private enum class ReimportIndicator {
+    /**
+     * Reimporting the target dataset is not possible due to the data being
+     * invalid or the state in MinIO being broken (missing import-ready.zip).
+     */
     ReimportNotPossible,
+
+    /**
+     * Reimport needed when:
+     * * A previous import attempt failed, but the cache-db does not have a
+     *   message recorded.  In this case we re-run to provide an error message
+     *   for the user.
+     * * The import is not marked as failed but we do not have both an
+     *   install-ready.zip file and vdi-manifest.json file
+     */
     NeedReimport,
+
+    /**
+     * We already have an import-ready.zip and vdi-manifest.json or the import
+     * previously failed and we have an error message for the user.
+     */
     ReimportNotNeeded,
   }
 
