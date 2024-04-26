dfba113cb83c7fa5dd1ee966896f31852add8175
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-04-26 15:21:41 -0400

    update plugin controller to remove auth requirement

--- service/daemon/rest-service/src/main/kotlin/org/veupathdb/service/vdi/server/controllers/PluginController.kt
+++ service/daemon/rest-service/src/main/kotlin/org/veupathdb/service/vdi/server/controllers/PluginController.kt
@@ -1,10 +1,8 @@
 package org.veupathdb.service.vdi.server.controllers
 
-import org.veupathdb.lib.container.jaxrs.server.annotations.Authenticated
 import org.veupathdb.service.vdi.generated.resources.VdiPlugins
 import org.veupathdb.service.vdi.service.plugins.listPlugins
 
-@Authenticated
 class PluginController : VdiPlugins {
   override fun getVdiPlugins(project: String?): VdiPlugins.GetVdiPluginsResponse {
     return VdiPlugins.GetVdiPluginsResponse.respond200WithApplicationJson(listPlugins(project))
