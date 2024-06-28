bdb05192a4d587da07775c5c2f7db9d962c365dc
Dan Galdi <dmgaldi@upenn.edu>
2024-06-28 10:40:23 -0400

    Revert "Update compose files"
    This reverts commit 1623da7bdb4f7adcf0166d18c6693e835fc3a948.

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     api("org.veupathdb.lib:multipart-jackson-pojo:1.1.7")
 
     // VDI
-    api("org.veupathdb.vdi:vdi-component-common:11.1.0")
+    api("org.veupathdb.vdi:vdi-component-common:11.0.0-dev-test-3")
     api("org.veupathdb.vdi:vdi-component-json:1.0.2")
 
     // Database
