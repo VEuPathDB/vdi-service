2fa0aa6ec30694acf58baf2236533429de47b594
Elizabeth Paige Harper <elizabeth.harper@foxcapades.io>
2025-03-25 11:04:40 -0400

    Revert "TEMPORARY HACK! REMOVE BEFORE TAGGING"
    This reverts commit 903259d41d9c520ac32b5fe3077a95c313ca99c8.

--- service/daemon/reconciler/src/main/kotlin/vdi/daemon/reconciler/ReconcilerImpl.kt
+++ service/daemon/reconciler/src/main/kotlin/vdi/daemon/reconciler/ReconcilerImpl.kt
@@ -21,31 +21,28 @@ internal class ReconcilerImpl(private val config: ReconcilerDaemonConfig, abortC
   }
 
   override suspend fun runJob() {
-    // FIXME: REMOVE THIS!!!!!!!! TEMPORARY HACK TO TEST SOMETHING ONE TIME!!!
-    return
-//
-//    val now = System.currentTimeMillis()
-//
-//    when {
-//      // delta between last full run and now is greater than or equal to the
-//      // full reconciler run interval
-//      (now - lastFullRun).milliseconds >= config.fullRunInterval -> {
-//        lastFullRun = now
-//
-//        // If the reconciler thread is disabled, just log a reminder.
-//        if (!config.reconcilerEnabled) {
-//          logger.info("reconciler disabled by config")
-//        } else {
-//          Recon.runFull()
-//        }
-//      }
-//
-//      // delta between last slim run and now is greater than or equal to the
-//      // slim reconciler run interval
-//      (now - lastSlimRun).milliseconds >= config.slimRunInterval -> {
-//        lastSlimRun = now
-//        Recon.runSlim()
-//      }
-//    }
+    val now = System.currentTimeMillis()
+
+    when {
+      // delta between last full run and now is greater than or equal to the
+      // full reconciler run interval
+      (now - lastFullRun).milliseconds >= config.fullRunInterval -> {
+        lastFullRun = now
+
+        // If the reconciler thread is disabled, just log a reminder.
+        if (!config.reconcilerEnabled) {
+          logger.info("reconciler disabled by config")
+        } else {
+          Recon.runFull()
+        }
+      }
+
+      // delta between last slim run and now is greater than or equal to the
+      // slim reconciler run interval
+      (now - lastSlimRun).milliseconds >= config.slimRunInterval -> {
+        lastSlimRun = now
+        Recon.runSlim()
+      }
+    }
   }
 }
