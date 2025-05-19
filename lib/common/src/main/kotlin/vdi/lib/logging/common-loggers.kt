c9f821c6d2dffa0c6e42b88de0ab4391ccebfa6e
Elizabeth Paige Harper <elizabeth.harper@foxcapades.io>
2025-05-19 13:15:48 -0400

    logging

--- lib/common/src/main/kotlin/vdi/lib/logging/common-loggers.kt
+++ lib/common/src/main/kotlin/vdi/lib/logging/common-loggers.kt
@@ -3,4 +3,4 @@ package vdi.lib.logging
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-val MetaLogger: Logger = LoggerFactory.getLogger("~")
+val MetaLogger: Logger = LoggerFactory.getLogger("init")
