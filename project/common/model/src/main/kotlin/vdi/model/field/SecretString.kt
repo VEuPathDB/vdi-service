5d32dbf02af95939ab5fdbfe983fcf3d3e807014
Elizabeth Paige Harper <elizabeth.harper@foxcapades.io>
2025-06-04 12:15:25 -0400

    secret string constructor

--- project/common/model/src/main/kotlin/vdi/model/field/SecretString.kt
+++ project/common/model/src/main/kotlin/vdi/model/field/SecretString.kt
@@ -2,6 +2,6 @@ package vdi.model.field
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 
-data class SecretString(@JsonIgnore val asString: String) {
+data class SecretString(@get:JsonIgnore val asString: String) {
   override fun toString() = "***"
 }
