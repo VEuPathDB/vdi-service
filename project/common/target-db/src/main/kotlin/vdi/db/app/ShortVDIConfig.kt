6bb9039aa2fb31f6b6dbaa21cc48bcca41608cbe
Elizabeth Paige Harper <elizabeth.harper@foxcapades.io>
2025-06-10 11:25:02 -0400

    wrapper annotation on short config pojo

--- project/common/target-db/src/main/kotlin/vdi/db/app/ShortVDIConfig.kt
+++ project/common/target-db/src/main/kotlin/vdi/db/app/ShortVDIConfig.kt
@@ -1,8 +1,14 @@
 package vdi.db.app
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import com.fasterxml.jackson.annotation.JsonTypeName
 import vdi.config.raw.common.LDAPConfig
 import vdi.config.raw.vdi.InstallTargetConfig
 
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
+@JsonTypeName(value = "vdi")
+@JsonIgnoreProperties(ignoreUnknown = true)
 internal data class ShortVDIConfig(
   val ldap: LDAPConfig?,
   val installTargets: Set<InstallTargetConfig>,
