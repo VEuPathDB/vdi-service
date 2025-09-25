46af31ef605a69588b44310a8078a29f27c9f0dd
Elizabeth Paige Harper <elizabeth.harper@foxcapades.io>
2025-09-25 16:24:53 -0400

    remove dead compose wrapper

--- utils/builder/go.mod
+++
@@ -1,5 +0,0 @@
-module builder
-
-go 1.25
-
-require github.com/Foxcapades/Argonaut v1.4.10
--- utils/source-me.sh
+++
@@ -1,9 +0,0 @@
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  alias builder=$PWD/utils/builder/bin/linux/builder
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-  alias builder=$PWD/utils/builder/bin/darwin/builder
-else
-  echo "sorry bucko, you're outta luck"
-fi
-
-
