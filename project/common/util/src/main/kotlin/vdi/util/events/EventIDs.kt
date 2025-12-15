97bf1cbc421a0ea282807daaa56ac259b9a8e870
Elizabeth Harper <elizabeth.harper@foxcapades.io>
2025-12-15 15:41:42 -0500

    reduce event id by origin timestamp

--- project/common/util/src/main/kotlin/vdi/util/events/EventIDs.kt
+++ project/common/util/src/main/kotlin/vdi/util/events/EventIDs.kt
@@ -3,10 +3,11 @@ package vdi.util.events
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import vdi.model.EventID
+import vdi.model.OriginTimestamp
 
 object EventIDs {
   private val mutex = Mutex(false)
-  private var eventID = System.currentTimeMillis().toULong()
+  private var eventID = (System.currentTimeMillis() - OriginTimestamp.toInstant().toEpochMilli()).toULong()
 
   suspend fun issueID(): EventID =
     mutex.withLock { return eventID++ }
