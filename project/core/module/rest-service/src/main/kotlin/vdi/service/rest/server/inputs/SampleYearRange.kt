2fbc56df581a08e221c6cda7cee32b36174e8acb
Elizabeth Harper <elizabeth.harper@foxcapades.io>
2026-06-02 13:28:30 -0400

    add validation check to prevent endYear < startYear in dataset characteristics

--- project/core/module/rest-service/src/main/kotlin/vdi/service/rest/server/inputs/SampleYearRange.kt
+++ project/core/module/rest-service/src/main/kotlin/vdi/service/rest/server/inputs/SampleYearRange.kt
@@ -1,9 +1,10 @@
-@file:JvmName("SampleYearRangeInputAdaptor")
 package vdi.service.rest.server.inputs
 
 import org.veupathdb.lib.request.validation.ValidationErrors
+import org.veupathdb.lib.request.validation.checkInRange
 import org.veupathdb.lib.request.validation.reqCheckInRange
 import org.veupathdb.lib.request.validation.rangeTo
+import org.veupathdb.lib.request.validation.require
 import vdi.model.meta.SampleYearRange
 import vdi.service.rest.generated.model.JsonField
 import vdi.service.rest.generated.model.SampleYearRange as APIYearRange
@@ -17,6 +18,15 @@ fun APIYearRange?.cleanup() =
 
 fun APIYearRange.validate(jPath: String, errors: ValidationErrors) {
   start.reqCheckInRange(jPath..JsonField.START, MinYear, MaxYear, errors)
+
+  val endPath = jPath..JsonField.END
+  end.require(endPath, errors) {
+    checkInRange(endPath, MinYear, MaxYear, errors)
+
+    if (end < (start ?: Short.MIN_VALUE)) {
+      errors.add(endPath, "end year must be greater than or equal to start year")
+    }
+  }
   end.reqCheckInRange(jPath..JsonField.END, MinYear, MaxYear, errors)
 }
 
