0522bcdce7e987a1a96474750ec64344c1da25aa
Elizabeth Harper <elizabeth.harper@foxcapades.io>
2026-09-01 10:17:53 -0400

    call request buffer read in loop to ensure all bytes get read

--- project/plugin-server/src/main/kotlin/vdi/service/plugin/util/multipart.kt
+++ project/plugin-server/src/main/kotlin/vdi/service/plugin/util/multipart.kt
@@ -30,7 +30,35 @@ fun <T: Any> PartData.parseAsJson(maxInputSize: ULong, type: KClass<T>): T =
   }
 
 private fun <T: Any> Source.parseAsJson(buffer: ByteArray, type: KClass<T>): T =
-  JSON.readValue(buffer, 0, readAtMostTo(buffer), type.java)
+  JSON.readValue(buffer, 0, repeatedReadAtMostTo(buffer), type.java)
 
 private fun <T: Any> Source.parseAsJson(type: KClass<T>): T =
   JSON.readValue(asInputStream(), type.java)
+
+/**
+ * Repeatedly calls [readAtMostTo] until the buffer is full or
+ *
+ * Depending on the underlying [Source] implementation, the [readAtMostTo]
+ * method may cap itself at 8KiB regardless of the buffer array size.
+ */
+private fun Source.repeatedReadAtMostTo(buffer: ByteArray): Int {
+  var totalRead = 0
+
+  while (totalRead < buffer.size) {
+    val singleRead = readAtMostTo(buffer, totalRead)
+
+    if (singleRead < 0) {
+      // if we got a -1 back from `readAtMostTo`, but never actually got any
+      // bytes for our buffer, pass up the -1.  Otherwise, break and return the
+      // total we did read.
+      if (totalRead == 0)
+        return -1
+
+      break
+    }
+
+    totalRead += singleRead
+  }
+
+  return totalRead
+}
